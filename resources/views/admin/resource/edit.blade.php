a17003aabb2ea6d157a8988d3e03ac4c7dea2a05
Sam Kitano <sam.kitano@gmail.com>
2019-12-31 12:01:45 +0000

    added delete action btn

--- resources/views/admin/resource/edit.blade.php
+++ resources/views/admin/resource/edit.blade.php
@@ -8,6 +8,17 @@
                     <i class="{{ $icon }}"></i> Edit {{ ucfirst($single) }} #{{ $data['id'] }}
                     @if(isset($data['name'])) - {{ $data['name'] }}@endif
 
+                    <button class="btn btn-delete btn-sm btn-danger hasTooltip float-right ml-1"
+                            type="button"
+                            title="Delete this {{ $single }}"
+                            data-toggle="modal"
+                            data-target="#deleteModal"
+                            data-name="{{ $data['name'] ?? '' }}"
+                            data-id="{{ $data->id }}"
+                            data-single="{{ $single }}"
+                            data-route="{{ route("Admin::{$resource}.destroy", $data->id) }}"
+                    ><i class="{{ \App\Util\Icons::_ICON_TRASH_ }}"></i></button>
+
                     <a href="{{ route("Admin::{$resource}.index") }}"
                        title="Go back to «{{ ucfirst($resource) }}» List"
                        class="btn btn-sm btn-primary float-right hasTooltip">
@@ -38,10 +49,6 @@
                             </div>
                         @endif
                     @endforeach
-
-                    {{-- TODO: check validation RULE--}}
-                    {{--Edit {{ $id }}--}}
-                   {{-- <input type="hidden" name="ignore[id]" value="{{ $data->id }}"> --}}
                 </div>
 
                 <div class="card-footer">
@@ -56,10 +63,13 @@
             </form>
         </div>
     </div>
+
+    @include('admin.modals.delete-modal')
 @endsection
 
 @push('postScripts')
     <script type="text/javascript">
-        appUtil.checkFormChanges(@json($data));
+        appUtil.checkFormChanges(@json($data))
+               .watchDelete()
     </script>
 @endpush
