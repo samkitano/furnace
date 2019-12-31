a37a01a313e0429d61fed0daa12e7f05acb79f27
Sam Kitano <sam.kitano@gmail.com>
2019-12-31 12:05:32 +0000

    added delete modal management

--- resources/js/base.js
+++ resources/js/base.js
@@ -1,6 +1,13 @@
+/**
+ * Activate BS tooltips
+ */
 $(function () {
   $(".hasTooltip").tooltip();
 })
+
+/**
+ * Sidebar Menu
+ */
 $('.dismiss, .overlay').on('click', function() {
   $('.sidebar').removeClass('active');
   $('.overlay').removeClass('active');
@@ -10,8 +17,42 @@ $('.open-menu').on('click', function(e) {
   e.preventDefault();
   $('.sidebar').toggleClass('active');
   $('.overlay').toggleClass('active');
-  // close opened sub-menus
+
   $('.collapse.show').toggleClass('show');
-  const exp = $('a[aria-expanded=true]').attr('aria-expanded')
-  $('a[aria-expanded=true]').attr('aria-expanded', !exp);
+
+  const $tgt = $('a[aria-expanded=true]')
+  const exp = $tgt.attr('aria-expanded')
+
+  $tgt.attr('aria-expanded', !exp);
 });
+
+/** Application Utilities */
+window.appUtil = {
+  checkFormChanges (data) {
+    // TODO
+    
+    console.log(data)
+    return this
+  },
+
+  loadDT (options) {
+    $('.dt').DataTable(options)
+
+    return this
+  },
+
+  watchDelete () {
+    $('#deleteModal').on('show.bs.modal', function (event) {
+      const button = $(event.relatedTarget)
+      const name = button.data('name')
+      const modal = $(this)
+
+      let msg = `Delete ${button.data('single')} #${button.data('id')}`
+
+      msg += name !== '' ? ` - «${name}»?` : '?'
+
+      modal.find('.modal-body').text(msg)
+      modal.find('form').attr('action', button.data('route'))
+    })
+  }
+};
