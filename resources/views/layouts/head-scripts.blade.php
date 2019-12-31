9413f51b0ff2c102f62fd4bd1e86647050f694aa
Sam Kitano <sam.kitano@gmail.com>
2019-12-31 12:05:57 +0000

    first commit

---
+++ resources/views/layouts/head-scripts.blade.php
@@ -0,0 +1,6 @@
+<script src="{{ asset('js/dt.js') }}"></script>
+<script src="{{ asset('js/pdfmake.js') }}"></script>
+<script src="{{ asset('js/vf_fonts.js') }}"></script>
+<script src="{{ asset('js/popper.js') }}"></script>
+<script src="{{ asset('js/bs.js') }}"></script>
+<script src="{{ asset('js/app.js') }}" defer></script>
