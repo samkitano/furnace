10c96637bca48c3beb47518e0512f381a96937b5
Sam Kitano <sam.kitano@gmail.com>
2020-01-01 18:10:24 +0000

    first commit

---
+++ app/Http/Requests/UpdateSubfamily.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSubfamily extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => "required|max:255|min:3|unique:subfamilies,name,{$this->route('subfamily')}",
+        ];
+    }
+}
